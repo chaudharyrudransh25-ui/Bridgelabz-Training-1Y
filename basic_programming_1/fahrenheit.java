a8122691c9e0e4769041e9abb7572f3551fdebf6
chaudharyrudransh25-ui <chaudharyrudransh25@gmail.com>
2026-01-19 10:10:43 +0530

    [Rudransh Chaudhari] remove file from mainbranch

--- basic_programming_1/fahrenheit.java
+++
@@ -1,9 +0,0 @@
-class fahrenheit
-{
-	public static void main(String args[])
-	{
-		int celcius = 5;
-		double Fahrenheit = (celcius * 9/5) + 32;
-		System.out.println("Fahrenheit :" + Fahrenheit);
-	}
-}
